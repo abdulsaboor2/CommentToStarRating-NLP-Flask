a051e7af90b9a3fea0b9095e270b8f37630e3718
abdulsaboor2 <absaboor019@gmail.com>
2022-08-10 18:09:48 +0500

    Coment to star rating

--- App.js
+++ App.js
@@ -1,51 +1,111 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, { useState } from 'react';
-import {
-  Button,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   const [text, setText] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  // To set the default Star Selected
+  const [rating, setRating] = useState();
+  // To set the max number of Stars
+  const [maxRating, setMaxRating] = useState([1, 2, 3, 4, 5]);
+ 
+  // Filled Star. You can also give the path from local
+  const starImageFilled =
+    'https://raw.githubusercontent.com/AboutReact/sampleresource/master/star_filled.png';
+  // Empty Star. You can also give the path from local
+  const starImageCorner =
+    'https://raw.githubusercontent.com/AboutReact/sampleresource/master/star_corner.png';
+
+    const handleSubmit = () => {
+      if(text == ""){
+        alert("Please Write Some Text on Text Box");
+      }
+      else{
+        getModelPrediction()
+        setVisible(true);
+      }
+    }
+
+    const getModelPrediction = async () => {
+     
+        try {
+          await fetch("http://192.168.10.10?q=" + text, { method: "GET" }).then((response) => response.json())
+            .then((responseJson) => {
+              setRating(responseJson);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+  
+        } catch (err) {
+          console.log("error  " + err);
+        }
+    };
+
+  const CustomRatingBar = () => {
+    return (
+      <View style={styles.customRatingBarStyle}>
+        {maxRating.map((item, key) => {
+          return (
+            <TouchableOpacity
+              activeOpacity={0.7}
+              key={item}>
+              <Image
+                style={styles.starImageStyle}
+                source={ item <= rating ? {uri: starImageFilled} : {uri: starImageCorner} }
+              />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  };
+
+  
 
   return (
     <SafeAreaView>
       <StatusBar backgroundColor="#61dafb" />
       <View>
-        <View>
-          <TextInput keyboardType='text' placeholder='Write Some Thing' onChangeText={(e)=>setText(e)} />
+        <View style={{marginTop:20, marginBottom:20, marginLeft:5, marginRight:5}}>
+          <Text style={{margin:10, fontSize:18, color:"black", fontWeight:"bold", }}>Write Comment Below</Text>
+          <TextInput style={{borderWidth:3, borderColor:"gray", fontSize:20, padding:10, minHeight:100, color: "black"}} multiline keyboardType='text' placeholder='Write Some Text' onChangeText={(e)=>setText(e)} />
         </View>
-        <Button title='Submit' onPress={()=>console.log("Do Something")} />
-        <Text>{text}</Text>
-      </View>
+        <View style={{padding:10}}>
+          <TouchableOpacity style={{backgroundColor:"lightgreen", height:45, borderRadius: 10, alignContent:"center", alignItems:"center", paddingTop:8}} onPress={handleSubmit}>
+            <Text style={{textAlign:"center", fontSize:21, fontWeight:"bold", color:"white"}}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+
+        { visible ? <CustomRatingBar /> : <></> }
+        
+
+      </View>   
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 10,
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
   
+  customRatingBarStyle: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  starImageStyle: {
+    width: 40,
+    height: 40,
+    resizeMode: 'cover',
+  },
 });
 
 export default App;
---
+++ venv/SentimentAnalysis.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+app = Flask(__name__)
+output = {}
+
+
+def sentiment(sentence):
+    nltk.download('vader_lexicon')
+    sid = SentimentIntensityAnalyzer()
+    score = sid.polarity_scores(sentence)['compound']
+
+    if score > 0.5:
+        return 5
+    elif score <= 0.5 and score > 0:
+        return 4
+    elif score == 0:
+        return 3
+    elif score >= -0.5 and score < 0:
+        return 2
+    else:
+        return 1
+
+
+@app.route('/', methods=['GET'])
+def sentimentRequest():
+    if request.method == "POST":
+        sentence = request.form['q']
+    else:
+        sentence = request.args.get('q')
+    sent = sentiment(sentence)
+    output['sentiment'] = sent
+    return jsonify(sent)
+
+
+if __name__ == "__main__":
+    # Replace the IP Address With your System Ip Address
+    app.run(host="192.168.10.10", port=80)
+    # app.run(debug=True)
